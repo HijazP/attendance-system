a1ec494b4068bb37dfd0645c516a08151fe0b190
HijazP <hanifaliramdani98@gmail.com>
2024-05-26 13:03:15 +0700

    update flask

--- app/frontend/camera_flask_app.py
+++ app/frontend/camera_flask_app.py
@@ -4,19 +4,14 @@ import datetime, time
 import os, sys
 import numpy as np
 from threading import Thread
+import fnmatch
 
-
-global capture,rec_frame, grey, switch, neg, face, rec, out 
+global capture, rec_frame, out
 capture=0
-grey=0
-neg=0
-face=0
-switch=1
-rec=0
 
 #make shots directory to save pics
 try:
-    os.mkdir('./shots')
+    os.mkdir('./static')
 except OSError as error:
     pass
 
@@ -25,7 +20,6 @@ net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_mod
 
 #instatiate flask app  
 app = Flask(__name__, template_folder='./templates')
-
 
 camera = cv2.VideoCapture(0)
 
@@ -62,28 +56,15 @@ def detect_face(frame):
  
 
 def gen_frames():  # generate frame by frame from camera
-    global out, capture,rec_frame
+    global out, capture, rec_frame
     while True:
         success, frame = camera.read() 
         if success:
-            if(face):                
-                frame= detect_face(frame)
-            if(grey):
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            if(neg):
-                frame=cv2.bitwise_not(frame)    
             if(capture):
                 capture=0
-                now = datetime.datetime.now()
-                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
-                cv2.imwrite(p, frame)
-            
-            if(rec):
-                rec_frame=frame
-                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
-                frame=cv2.flip(frame,1)
-            
-                
+                frame= detect_face(frame)
+                p = os.path.sep.join(['static', data])
+                check = cv2.imwrite(p, frame)
             try:
                 ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
                 frame = buffer.tobytes()
@@ -100,55 +81,32 @@ def gen_frames():  # generate frame by frame from camera
 def index():
     return render_template('index.html')
     
-    
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/requests',methods=['POST','GET'])
 def tasks():
-    global switch,camera
+    global switch,camera,data,check
     if request.method == 'POST':
-        if request.form.get('click') == 'Capture':
+        if request.form.get('click') == 'Presensi':
             global capture
+            now = datetime.datetime.now()
+            data = "shot_{}.png".format(str(now).replace(":",'.').replace(" ",'_'))
+            check=False
             capture=1
-        elif  request.form.get('grey') == 'Grey':
-            global grey
-            grey=not grey
-        elif  request.form.get('neg') == 'Negative':
-            global neg
-            neg=not neg
-        elif  request.form.get('face') == 'Face Only':
-            global face
-            face=not face 
-            if(face):
-                time.sleep(4)   
-        elif  request.form.get('stop') == 'Stop/Start':
-            
-            if(switch==1):
-                switch=0
-                camera.release()
-                cv2.destroyAllWindows()
-                
-            else:
-                camera = cv2.VideoCapture(0)
-                switch=1
-        elif  request.form.get('rec') == 'Start/Stop Recording':
-            global rec, out
-            rec= not rec
-            if(rec):
-                now=datetime.datetime.now() 
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
-                #Start new thread for recording the video
-                thread = Thread(target = record, args=[out,])
-                thread.start()
-            elif(rec==False):
-                out.release()
+            while(check!=False):
+                pass
+            return render_template('absen.html', value=data)
+
+        if request.form.get('presensi-ulang') == 'Presensi Ulang':
+            os.remove('./static/'+data)
+            data=''
+            return render_template('index.html')
                           
-                 
     elif request.method=='GET':
         return render_template('index.html')
+
     return render_template('index.html')
 
 
